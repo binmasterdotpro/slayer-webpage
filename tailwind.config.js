cf461234e68839aeaf75ac6f2928f92e65691f61
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2023-12-14 20:01:10 -0500

    chat done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './src/**/*.{html,js}'
+    ],
+    theme: {
+        extend: {
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            }
+        },
+    },
+    plugins: [],
 }
 
